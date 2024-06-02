b34e22169aeac6e7c0173e779640e5dc8f562bc1
PrittoRuban <prittoruban0609@gmail.com>
2024-06-02 21:04:18 +0530

    update code

--- CalculateGPA.java
+++ CalculateGPA.java
@@ -17,23 +17,18 @@ public class CalculateGPA {
 
     public static double calculateGPA(String grade) {
         switch (grade) {
-            case "A" -> {
-                return 4.0;
-            }
-            case "B" -> {
-                return 3.0;
-            }
-            case "C" -> {
-                return 2.0;
-            }
-            case "D" -> {
-                return 1.0;
-            }
-            case "F" -> {
-                return 0.0;
-            }
-            default ->
-                throw new IllegalArgumentException("Invalid grade. Please enter a grade from A to F.");
+            case "A":
+            return 4.0;
+            case "B":
+            return 3.0;
+            case "C":
+            return 2.0;
+            case "D":
+            return 1.0;
+            case "F":
+            return 0.0;
+            default:
+            throw new IllegalArgumentException("Invalid grade. Please enter a grade from A to F.");
         }
     }
 }
